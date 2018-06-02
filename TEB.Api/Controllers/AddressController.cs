6b20d89217383f1f8d0731bd548b6b09a8eb815a
AbdulDayyan-RFI <30667908+AbdulDayyan-RFI@users.noreply.github.com>
2018-06-02 09:55:17 +0530

    adreess insert

--- TEB.Api/Controllers/AddressController.cs
+++ TEB.Api/Controllers/AddressController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TEB.Core.Domain;
+using TEB.Core.ViewModel;
 using TEB.Service;
 
 namespace TEB.Api.Controllers
@@ -22,6 +24,21 @@ namespace TEB.Api.Controllers
             return RunInSafe(() =>
             {
                 var data = _addressService.GetAllAddressByCustomerId(Id);
+                tebResponse.Data = data;
+                tebResponse.IsSuccess = true;
+                return Ok(tebResponse);
+            });
+        }
+
+        [HttpPost]
+        public IHttpActionResult InsertAddress(AddressViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return RunInSafe(() =>
+            {
+                var data = _addressService.AddCustomerAddress(model);
                 tebResponse.Data = data;
                 tebResponse.IsSuccess = true;
                 return Ok(tebResponse);
