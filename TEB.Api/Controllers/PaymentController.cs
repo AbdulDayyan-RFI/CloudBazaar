1989f3e4ff25776a2f5d1eaf3d0b51455ee46ab0
faraaz <faraaz614@gmail.com>
2018-07-22 20:56:52 +0530

    braintree payment

---
+++ TEB.Api/Controllers/PaymentController.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Http;
+using TEB.Service;
+
+namespace TEB.Api.Controllers
+{
+    [AllowAnonymous]
+    public class PaymentController : BaseApiController
+    {
+        public readonly IPaymentService _paymentService;
+        
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GenerateClientToken(string aCustomerId, string aMerchantAccountid)
+        {
+            return RunInSafe(() =>
+            {
+                var data = _paymentService.GenerateClientToken(aCustomerId, aMerchantAccountid);
+                tebResponse.Data = data;
+                tebResponse.IsSuccess = true;
+                return Ok(tebResponse);
+            });
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreatePurchase(string nonceFromTheClient)
+        {
+            return RunInSafe(() =>
+            {
+                var data = _paymentService.CreatePurchase(nonceFromTheClient);
+                tebResponse.Data = data;
+                tebResponse.IsSuccess = true;
+                return Ok(tebResponse);
+            });
+        }
+    }
+}
---
+++ TEB.Service/Payment/PaymentService.cs
@@ -0,0 +1,50 @@
+﻿using Braintree;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEB.Service
+{
+    public class PaymentService : IPaymentService
+    {
+        BraintreeGateway gateway = new BraintreeGateway();
+
+        public PaymentService()
+        {
+            gateway = new BraintreeGateway
+            {
+                Environment = ConfigurationManager.AppSettings["Environment"] == "SandBox" ? Braintree.Environment.SANDBOX : Braintree.Environment.PRODUCTION,
+                MerchantId = ConfigurationManager.AppSettings["merchant_id"],
+                PublicKey = ConfigurationManager.AppSettings["public_key"],
+                PrivateKey = ConfigurationManager.AppSettings["private_key"]
+            };
+        }
+
+        public string GenerateClientToken(string aCustomerId, string aMerchantAccountid)
+        {
+            var clientToken = gateway.ClientToken.Generate(new ClientTokenRequest { CustomerId = aCustomerId });
+            return clientToken;
+        }
+
+        public string CreatePurchase(string nonceFromTheClient)
+        {
+            var request = new TransactionRequest
+            {
+                Amount = 10.00M,
+                PaymentMethodNonce = nonceFromTheClient,
+                Options = new TransactionOptionsRequest
+                {
+                    SubmitForSettlement = true
+                }
+            };
+
+            Result<Transaction> result = gateway.Transaction.Sale(request);
+            //save in db here
+
+            return result.Message;
+        }
+    }
+}
